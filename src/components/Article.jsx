122e7525c49881dc1673622a76c96084941d95ac
muazaiman45 <87751717+muazmn@users.noreply.github.com>
2023-09-17 10:18:24 +0800

    JSX

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,5 +1,21 @@
+/* eslint-disable react/jsx-key */
 function Article(){
-    return <div>this is component</div>
+const name = "muaz"
+const titles = ['ahmad', 'muaz', 'aiman'];
+    // return with square bracket and fragment used when we wanna return html elem more than one
+    return (
+        <>
+        <div>{name}</div>
+        <div>{titles.map((title) => {
+            return (
+                <div>{title}</div>
+            )
+        })}</div>
+        </>
+    )
+
+    // syntax below just allows us return one html element
+    // return <div>this is component</div>
 }
 
 // need to export to enable us to use this component on other file
