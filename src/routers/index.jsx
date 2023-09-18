551574519f62f22f034b96d19f1458a411ba9f48
muazaiman45 <87751717+muazmn@users.noreply.github.com>
2023-09-18 22:57:31 +0800

    add routing for about page

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable no-unused-vars */
 import {createBrowserRouter} from 'react-router-dom';
 import Home from '../pages';
+import About from '../pages/About';
 
 export const router = createBrowserRouter([
     // below is the list of routing system, the the url just "/", so we will redirect to home
     {
         path: '/',
         element:<Home/>
+    },
+    {
+        path: "/about",
+        element: <About/>
     }
 ])
