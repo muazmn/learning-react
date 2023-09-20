b84f34fd91b28c07e1262bade9290f4288dca5d6
muazaiman45 <87751717+muazmn@users.noreply.github.com>
2023-09-20 20:30:42 +0800

    navlink

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,13 @@
 // outlet is a identifier or a component that will grouping all pages, and outlet used when we navigate to other page, we want the context also will be rendered
-import { Link, Outlet} from "react-router-dom";
+import { NavLink, Outlet} from "react-router-dom";
+import '../styles/index.css'
 
 
 function RootLayout() {
     return (
         <>
-        <Link to='/'>Home</Link> | <Link to='/blog'>Blog</Link> | <Link to="/about">About</Link>
+        {/* use navlink if we want give effect for the active link, by default an active class is added to NavLink when is it active */}
+        <NavLink to='/'>Home</NavLink> | <NavLink to='/blog'>Blog</NavLink> | <NavLink to="/about">About</NavLink>
         {/* it means all pages those are children of this page, put in the element Outlet */}
         <Outlet/>
         </>
